0dc84b629d127303449c36372a868095097ea83e
Jamie Cox <me@jcox.tech>
2019-02-15 15:43:35 +0000

    Wrap tests in unittest test case and run in travis

--- test/test_rabbit_helper.py
+++ test/test_rabbit_helper.py
@@ -1,73 +1,90 @@
+import os
 from unittest.mock import patch, MagicMock
+from unittest import TestCase
 
-import pika
 from pika.exceptions import AMQPConnectionError
 from pytest import raises
 
-from app.rabbit_helper import init_rabbitmq, send_message_to_rabbitmq
-from test.helpers.when_then_return import *
+from test.helpers.when_then_return import create_when_then_return, create_when_then_return_kwargs
 
-RM_PROPERTY_CLASS = 'property_class'
 
-RM_RABBIT_URL = 'rabbit_url'
 RM_RABBIT_AMQP = "amqp://user:pa55word@host:0001"
-RM_BINDING_KEY = "Case.Responses.binding"
+RM_BINDING_KEY = "Test.binding"
 RM_RABBIT_QUEUE = "Test.Queue"
-RM_RABBIT_CONNECTION = 'rabbit connection'
 RM_RABBIT_EXCHANGE = "test-exchange"
-RM_MESSAGE = "xml message<blah>"
-RM_RABBIT_QUEUE_ARGS = {'x-dead-letter-exchange': 'case-deadletter-exchange',
-                        'x-dead-letter-routing-key': RM_BINDING_KEY}
 
 
-def test_rabbit_init():
-    with patch('app.rabbit_helper.pika') as mock_pika:
-        connection_mock = MagicMock()
-        mock_pika.URLParameters = create_when_then_return(RM_RABBIT_AMQP, return_value=RM_RABBIT_URL)
-        mock_pika.BlockingConnection = create_when_then_return(RM_RABBIT_URL, return_value=connection_mock)
+class RabbitHelperTestCase(TestCase):
 
-        channel_mock = MagicMock()
-        connection_mock.channel = create_when_then_return(return_value=channel_mock)
+    property_class = 'property_class'
+    rabbit_url = 'rabbit_url'
+    rabbit_connection = 'rabbit connection'
+    message = "xml message<blah>"
+    queue_args = {'x-dead-letter-exchange': 'case-deadletter-exchange',
+                  'x-dead-letter-routing-key': RM_BINDING_KEY}
 
-        init_rabbitmq(rabbitmq_amqp=RM_RABBIT_AMQP,
-                      binding_key=RM_BINDING_KEY,
-                      exchange_name=RM_RABBIT_EXCHANGE,
-                      queue_name=RM_RABBIT_QUEUE)
+    def setUp(self):
+        test_environment_variables = {
+            'RABBIT_AMQP': RM_RABBIT_AMQP,
+            'RABBIT_ROUTING_KEY': RM_BINDING_KEY,
+            'RABBIT_QUEUE': RM_RABBIT_QUEUE,
+            'RABBIT_EXCHANGE': RM_RABBIT_EXCHANGE,
+        }
+        os.environ.update(test_environment_variables)
 
-        channel_mock.exchange_declare.assert_called_once_with(exchange=RM_RABBIT_EXCHANGE, exchange_type='direct',
-                                                              durable=True)
-        channel_mock.queue_declare.assert_called_once_with(arguments=RM_RABBIT_QUEUE_ARGS, durable=True,
-                                                           queue=RM_RABBIT_QUEUE)
-        channel_mock.queue_bind(exchange=RM_RABBIT_EXCHANGE, queue=RM_RABBIT_QUEUE, routing_key=RM_BINDING_KEY)
+    def test_rabbit_init(self):
+        from app.rabbit_helper import init_rabbitmq
 
+        with patch('app.rabbit_helper.pika') as mock_pika:
+            connection_mock = MagicMock()
+            mock_pika.URLParameters = create_when_then_return(RM_RABBIT_AMQP, return_value=self.rabbit_url)
+            mock_pika.BlockingConnection = create_when_then_return(self.rabbit_url, return_value=connection_mock)
 
-def test_initialise_messaging_rabbit_fails():
-    with raises(AMQPConnectionError):
-        with patch('pika.BlockingConnection', side_effect=AMQPConnectionError):
-            init_rabbitmq()
+            channel_mock = MagicMock()
+            connection_mock.channel = create_when_then_return(return_value=channel_mock)
 
+            init_rabbitmq(rabbitmq_amqp=RM_RABBIT_AMQP,
+                          binding_key=RM_BINDING_KEY,
+                          exchange_name=RM_RABBIT_EXCHANGE,
+                          queue_name=RM_RABBIT_QUEUE,
+                          queue_args=self.queue_args)
 
-def test_send_to_rabbitmq_queue():
-    with patch('app.rabbit_helper.pika') as mock_pika:
-        connection_mock = MagicMock()
-        mock_pika.URLParameters = create_when_then_return(RM_RABBIT_AMQP, return_value=RM_RABBIT_URL)
-        mock_pika.BlockingConnection = create_when_then_return(RM_RABBIT_URL, return_value=connection_mock)
+            channel_mock.exchange_declare.assert_called_once_with(exchange=RM_RABBIT_EXCHANGE, exchange_type='direct',
+                                                                  durable=True)
+            channel_mock.queue_declare.assert_called_once_with(arguments=self.queue_args, durable=True,
+                                                               queue=RM_RABBIT_QUEUE)
+            channel_mock.queue_bind(exchange=RM_RABBIT_EXCHANGE, queue=RM_RABBIT_QUEUE, routing_key=RM_BINDING_KEY)
 
-        channel_mock = MagicMock()
-        connection_mock.channel = create_when_then_return(return_value=channel_mock)
-        mock_pika.BasicProperties = create_when_then_return_kwargs({'content_type': 'text/xml'},
-                                                                   return_value=RM_PROPERTY_CLASS)
+    def test_initialise_messaging_rabbit_fails(self):
+        from app.rabbit_helper import init_rabbitmq
 
-        send_message_to_rabbitmq(RM_MESSAGE,
-                                 rabbitmq_amqp=RM_RABBIT_AMQP,
-                                 exchange_name=RM_RABBIT_EXCHANGE,
-                                 routing_key=RM_BINDING_KEY)
+        with raises(AMQPConnectionError):
+            with patch('pika.BlockingConnection', side_effect=AMQPConnectionError):
+                init_rabbitmq()
 
-        channel_mock.basic_publish.assert_called_once_with(exchange=RM_RABBIT_EXCHANGE,
-                                                           routing_key=RM_BINDING_KEY,
-                                                           body=str(RM_MESSAGE),
-                                                           properties=RM_PROPERTY_CLASS)
-        connection_mock.close.assert_called_once()
+    def test_send_to_rabbitmq_queue(self):
+        from app.rabbit_helper import send_message_to_rabbitmq
+
+        with patch('app.rabbit_helper.pika') as mock_pika:
+            connection_mock = MagicMock()
+            mock_pika.URLParameters = create_when_then_return(RM_RABBIT_AMQP, return_value=self.rabbit_url)
+            mock_pika.BlockingConnection = create_when_then_return(self.rabbit_url, return_value=connection_mock)
+
+            channel_mock = MagicMock()
+            connection_mock.channel = create_when_then_return(return_value=channel_mock)
+            mock_pika.BasicProperties = create_when_then_return_kwargs({'content_type': 'text/xml'},
+                                                                       return_value=self.property_class)
+
+            send_message_to_rabbitmq(self.message,
+                                     rabbitmq_amqp=RM_RABBIT_AMQP,
+                                     exchange_name=RM_RABBIT_EXCHANGE,
+                                     routing_key=RM_BINDING_KEY)
+
+            channel_mock.basic_publish.assert_called_once_with(exchange=RM_RABBIT_EXCHANGE,
+                                                               routing_key=RM_BINDING_KEY,
+                                                               body=str(self.message),
+                                                               properties=self.property_class)
+            connection_mock.close.assert_called_once()
 
 
 # TODO
--- test/test_subscriber.py
+++ test/test_subscriber.py
@@ -1,73 +1,95 @@
+import os
+from unittest import TestCase
 from unittest.mock import patch, MagicMock
-from app.subscriber import setup_subscription, receipt_to_case
+
 from test.helpers.when_then_return import create_when_then_return
 
-TEST_DATA = 'test-data'
 
-SUBSCRIPTION_PATH = 'test-subscription-path'
-TOPIC_PATH = 'test-topic-path'
 RECEIPT_TOPIC_NAME = 'test-topic'
 RECEIPT_TOPIC_PROJECT_ID = 'test-project'
 SUBSCRIPTION_NAME = 'test-subscription'
 SUBSCRIPTION_PROJECT_ID = 'test-project-id'
-SUBSCRIBER_FUTURE = 'test-future'
-RM_BUCKET = 'test-bucket'
-RM_OBJECT_ID = 'test-object'
 
 
-def test_subscription_set_up():
-    with patch('app.subscriber.client') as mock_client:
-        callback_func = create_when_then_return(None, return_value=None)
+class TestSubscriber(TestCase):
+    gcp_bucket = 'test-bucket'
+    gcp_object_id = 'test-object'
+    subscriber_future = 'test-future'
+    subscription_path = 'test-subscription-path'
+    test_data = 'test-data'
+    topic_path = 'test-topic-path'
 
-        mock_client.topic_path = create_when_then_return(RECEIPT_TOPIC_PROJECT_ID, RECEIPT_TOPIC_NAME,
-                                                         return_value=TOPIC_PATH)
-        mock_client.subscription_path = create_when_then_return(SUBSCRIPTION_PROJECT_ID, SUBSCRIPTION_NAME,
-                                                                return_value=SUBSCRIPTION_PATH)
-        mock_client.create_subscription = create_when_then_return(SUBSCRIPTION_PATH, TOPIC_PATH, return_value=None)
-        mock_client.subscribe = create_when_then_return(SUBSCRIPTION_PATH, callback_func,
-                                                        return_value=SUBSCRIBER_FUTURE)
+    def setUp(self):
+        test_environment_variables = {
+            'RECEIPT_TOPIC_NAME': RECEIPT_TOPIC_NAME,
+            'RECEIPT_TOPIC_PROJECT_ID': RECEIPT_TOPIC_PROJECT_ID,
+            'SUBSCRIPTION_NAME': SUBSCRIPTION_NAME,
+            'SUBSCRIPTION_PROJECT_ID': SUBSCRIPTION_PROJECT_ID,
+        }
+        os.environ.update(test_environment_variables)
 
-        actual_future = setup_subscription(subscription_name=SUBSCRIPTION_NAME,
-                                           subscription_project_id=SUBSCRIPTION_PROJECT_ID,
-                                           topic_name=RECEIPT_TOPIC_NAME,
-                                           topic_project_id=RECEIPT_TOPIC_PROJECT_ID,
-                                           callback=callback_func)
+    def test_subscription_set_up(self):
+        from app.subscriber import setup_subscription
 
-    assert actual_future == SUBSCRIBER_FUTURE
+        with patch('app.subscriber.client') as mock_client:
+            callback_func = create_when_then_return(None, return_value=None)
 
+            mock_client.topic_path = create_when_then_return(RECEIPT_TOPIC_PROJECT_ID, RECEIPT_TOPIC_NAME,
+                                                             return_value=self.topic_path)
+            mock_client.subscription_path = create_when_then_return(SUBSCRIPTION_PROJECT_ID, SUBSCRIPTION_NAME,
+                                                                    return_value=self.subscription_path)
+            mock_client.create_subscription = create_when_then_return(self.subscription_path, self.topic_path,
+                                                                      return_value=None)
+            mock_client.subscribe = create_when_then_return(self.subscription_path, callback_func,
+                                                            return_value=self.subscriber_future)
 
-def test_receipt_to_case():
-    with patch('app.subscriber.send_message_to_rabbitmq') as mock_send_message_to_rabbit_mq:
-        mock_message = MagicMock()
-        mock_message.attributes = {'eventType': 'OBJECT_FINALIZE', 'bucketId': RM_BUCKET, 'objectId': RM_OBJECT_ID}
-        mock_message.data = TEST_DATA
+            actual_future = setup_subscription(subscription_name=SUBSCRIPTION_NAME,
+                                               subscription_project_id=SUBSCRIPTION_PROJECT_ID,
+                                               topic_name=RECEIPT_TOPIC_NAME,
+                                               topic_project_id=RECEIPT_TOPIC_PROJECT_ID,
+                                               callback=callback_func)
 
-        with patch('app.subscriber.json') as mock_json:
-            payload = {'timeCreated': '2018-08-24T00:00:00Z'}
-            mock_json.loads = create_when_then_return(TEST_DATA, return_value=payload)
+        assert actual_future == self.subscriber_future
 
-            receipt_to_case(mock_message)
+    def test_receipt_to_case(self):
+        from app.subscriber import receipt_to_case
 
-            expected_msg = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' \
-                           '<ns2:caseReceipt xmlns:ns2="http://ons.gov.uk/ctp/response/casesvc/message/feedback">'\
-                           '<caseId>test-object</caseId><inboundChannel>OFFLINE</inboundChannel>' \
-                           '<responseDateTime>2018-08-24T00:00:00+00:00</responseDateTime></ns2:caseReceipt>'
+        with patch('app.subscriber.send_message_to_rabbitmq') as mock_send_message_to_rabbit_mq:
+            mock_message = MagicMock()
+            mock_message.attributes = {'eventType': 'OBJECT_FINALIZE',
+                                       'bucketId': self.gcp_bucket,
+                                       'objectId': self.gcp_object_id}
+            mock_message.data = self.test_data
 
-            mock_send_message_to_rabbit_mq.assert_called_once_with(expected_msg)
-            mock_message.ack.assert_called_once()
+            with patch('app.subscriber.json') as mock_json:
+                payload = {'timeCreated': '2018-08-24T00:00:00Z'}
+                mock_json.loads = create_when_then_return(self.test_data, return_value=payload)
 
+                receipt_to_case(mock_message)
 
-def test_receipt_to_case_key_error():
-    with patch('app.subscriber.send_message_to_rabbitmq') as mock_send_message_to_rabbit_mq:
-        mock_message = MagicMock()
-        mock_message.attributes = {'TypoMissingKeyError': 'OBJECT_FINALIZE', 'bucketId': RM_BUCKET, 'objectId': RM_OBJECT_ID}
-        mock_message.data = TEST_DATA
+                expected_msg = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' \
+                               '<ns2:caseReceipt xmlns:ns2="http://ons.gov.uk/ctp/response/casesvc/message/feedback">' \
+                               '<caseId>test-object</caseId><inboundChannel>OFFLINE</inboundChannel>' \
+                               '<responseDateTime>2018-08-24T00:00:00+00:00</responseDateTime></ns2:caseReceipt>'
 
-        with patch('app.subscriber.json') as json_mock:
-            payload = {'timeCreated': '2018-08-24T00:00:00Z'}
-            json_mock.loads = create_when_then_return(TEST_DATA, return_value=payload)
+                mock_send_message_to_rabbit_mq.assert_called_once_with(expected_msg)
+                mock_message.ack.assert_called_once()
 
-            receipt_to_case(mock_message)
+    def test_receipt_to_case_key_error(self):
+        from app.subscriber import receipt_to_case
 
-            mock_send_message_to_rabbit_mq.assert_not_called()
-            mock_message.ack.assert_not_called()
+        with patch('app.subscriber.send_message_to_rabbitmq') as mock_send_message_to_rabbit_mq:
+            mock_message = MagicMock()
+            mock_message.attributes = {'TypoMissingKeyError': 'OBJECT_FINALIZE',
+                                       'bucketId': self.gcp_bucket,
+                                       'objectId': self.gcp_object_id}
+            mock_message.data = self.test_data
+
+            with patch('app.subscriber.json') as json_mock:
+                payload = {'timeCreated': '2018-08-24T00:00:00Z'}
+                json_mock.loads = create_when_then_return(self.test_data, return_value=payload)
+
+                receipt_to_case(mock_message)
+
+                mock_send_message_to_rabbit_mq.assert_not_called()
+                mock_message.ack.assert_not_called()
