30d6d164d5001ad225136126b1eede3e35d2b04d
AdamHawtin <adam.hawtin@ons.gov.uk>
2019-02-18 10:16:16 +0000

    Add logging to readiness, small refactor

--- app/readiness.py
+++ app/readiness.py
@@ -1,21 +1,22 @@
+import logging
 import os
+
+from structlog import wrap_logger
+
+logger = wrap_logger(logging.getLogger(__name__))
 
 
 class Readiness:
     def __init__(self, readiness_file):
         self.readiness_file = readiness_file
-        self._is_ready = False
 
     def __enter__(self):
-        self.make_ready()
+        logger.debug('Creating readiness file')
+        open(self.readiness_file, 'w').close()
 
     def __exit__(self, *args):
-        self.make_unready()
-
-    def make_ready(self):
-        with open(self.readiness_file, 'w'):
-            self._is_ready = True
-
-    def make_unready(self):
-        os.remove(self.readiness_file)
-        self._is_ready = False
+        logger.debug('Removing readiness file')
+        try:
+            os.remove(self.readiness_file)
+        except FileNotFoundError:
+            logger.error('Readiness file not found on exit')
