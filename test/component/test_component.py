09d7aefe9f4f6d5c2cb9bbe5c46722a5c577ae8a
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-02-25 09:14:20 +0000

    small refactor to tests and changed README

--- test/component/test_component.py
+++ test/component/test_component.py
@@ -7,13 +7,11 @@ from coverage.python import os
 from google.cloud import pubsub_v1
 
 RABBIT_AMQP = "amqp://guest:guest@localhost:35672"
-SUBSCRIPTION_PROJECT_ID = "project"
 RECEIPT_TOPIC_PROJECT_ID = "project"
 RABBIT_QUEUE = "Case.Responses"
 RABBIT_EXCHANGE = "case-outbound-exchange"
 RABBIT_ROUTE = "Case.Responses.binding"
 RECEIPT_TOPIC_NAME = "eq-submission-topic"
-SUBSCRIPTION_NAME = "rm-receipt-subscription"
 RABBIT_QUEUE_ARGS = {'x-dead-letter-exchange': 'case-deadletter-exchange',
                      'x-dead-letter-routing-key': RABBIT_ROUTE}
 
@@ -24,20 +22,22 @@ class CensusRMPubSubComponentTest(TestCase):
         os.environ["PUBSUB_EMULATOR_HOST"] = "localhost:8538"
         self.purge_rabbit_queue()
 
-    def test_e2e_good(self):
+    def test_e2e_with_sucessful_msg(self):
         expected_object_id = str(uuid.uuid4())
         self.publish_to_pubsub(expected_object_id)
 
-        expected_msg = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' \
-                       + '<ns2:caseReceipt xmlns:ns2="http://ons.gov.uk/ctp/response/casesvc/message/feedback">' \
-                       + '<caseId>' + expected_object_id + '</caseId><inboundChannel>OFFLINE</inboundChannel>' \
-                       + '<responseDateTime>2008-08-24T00:00:00+00:00</responseDateTime></ns2:caseReceipt>'
+        expected_msg = (
+                        f'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'
+                        f'<ns2:caseReceipt xmlns:ns2="http://ons.gov.uk/ctp/response/casesvc/message/feedback">'
+                        f'<caseId>{expected_object_id}</caseId><inboundChannel>OFFLINE</inboundChannel>'
+                        f'<responseDateTime>2008-08-24T00:00:00+00:00</responseDateTime></ns2:caseReceipt>'
+        )
 
         channel, queue_declare_result = self.init_rabbitmq()
-        assert queue_declare_result.method.message_count == 1
+        assert queue_declare_result.method.message_count == 1, "Expected 1 msg to be on rabbitmq queue"
 
         actual_msg_body_str = self.get_msg_body_from_rabbit(channel)
-        assert expected_msg == actual_msg_body_str
+        assert expected_msg == actual_msg_body_str, "Msg text incorrect"
 
     def purge_rabbit_queue(self):
         channel, queue_declare_result = self.init_rabbitmq()
